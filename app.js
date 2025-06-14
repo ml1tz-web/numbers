2994e4ef90cbf24aaf0f2ea75027cd7372781f59
Dima <kole0038@hz.nl>
2025-06-14 22:12:47 +0100

    Fix

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const numbers = [42, 7, 13, 21, 100, 365];
 // Retrievs a fact about a number
 async function getNumberFact(number, type = '') {
     try {
-        const url = `https://numbersapi.com/${number}${type ? '/' + type : ''}?json`;
+        const apiUrl = `http://numbersapi.com/${number}${type ? '/' + type : ''}?json`;
+        const url = `https://api.allorigins.win/raw?url=${encodeURIComponent(apiUrl)}`;
         const response = await fetch(url);
         
         if (!response.ok) {
